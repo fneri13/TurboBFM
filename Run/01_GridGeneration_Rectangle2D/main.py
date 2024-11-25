fixed computation of dual volumes without ghost cells

Run/01_GridGeneration_Rectangle2D/main.py
@@ -14,7 +14,7 @@ Generate a 2D rectangular geometry, that will be used as verification
 OUTPUT_FOLDER = 'Grid'
 NX = 20
 NY = 10
-NZ = 2
+NZ = 5
 
 x = np.linspace(0, NX-1, NX)
 y = np.linspace(0, NY-1, NY)

Test/test_CMesh.py
@@ -1,6 +1,6 @@
 import unittest
 import numpy as np
-from TurboBFM.Solver.CGrid import CGrid
+from TurboBFM.Solver.CElement import CGrid
 from TurboBFM.Solver.CMesh import CMesh
 
 x = np.linspace(0 , 10, 11)