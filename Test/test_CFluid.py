454833ddefde2a2aad05d5a9648e854ed57423a5
Francesco Neri <francesconeri95@gmail.com>
2024-11-29 10:27:20 +0100

    completed fluid tests

--- Test/test_CFluid.py
+++ Test/test_CFluid.py
@@ -5,7 +5,8 @@ from TurboBFM.Solver.CConfig import CConfig
 
 # test values
 gmma = 1.5
-fluid = FluidIdeal(gmma)
+R = 100
+fluid = FluidIdeal(gmma, R)
 
 class TestFluid(unittest.TestCase):
     def test_ComputeStaticEnergy_p_rho(self):
@@ -50,6 +51,29 @@ class TestFluid(unittest.TestCase):
         et = 1
         p = fluid.ComputePressure_rho_u_et(rho, u, et)
         self.assertGreater(p, 0)
+
+    def test_ComputeTotalEnthalpy_rho_u_et(self):
+        rho = 1
+        u = np.array([1, 0, 0])
+        et = 1
+        ht = fluid.ComputeTotalEnthalpy_rho_u_et(rho, u, et)
+        self.assertEqual(ht, 1.25)
+    
+    def test_ComputeStaticPressure_pt_M(self):
+        pt = 1
+        M = 0.1
+        p = fluid.ComputeStaticPressure_pt_M(pt, M)
+        p_ref = pt*(1+(gmma-1)/2*M**2)**(-gmma/(gmma-1))
+        self.assertEqual(p, p_ref)
+    
+    def test_ComputeStaticTemperature_Tt_M(self):
+        Tt = 3
+        M = 0.3
+        T = fluid.ComputeStaticTemperature_Tt_M(Tt, M)
+        T_ref = Tt*(1+(gmma-1)/2*M**2)**(-1)
+        self.assertEqual(T, T_ref)
+    
+
 
 
         
