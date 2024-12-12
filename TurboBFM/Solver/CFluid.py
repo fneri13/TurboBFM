9bd543f36144034de2142a41f6b06da7c17a52ab
Francesco Neri <francesconeri95@gmail.com>
2024-12-12 11:56:27 +0100

    checked the equations

--- TurboBFM/Solver/CFluid.py
+++ TurboBFM/Solver/CFluid.py
@@ -8,18 +8,34 @@ class FluidIdeal():
     Ideal Fluid Class, where thermodynamic properties and transformation are computed with ideal gas laws
     """
     def __init__(self, gmma, R):
+        """
+        Initialize fluid element. 
+
+        Parameters
+        -----------------------
+
+        `gmma`: cp/cv ratio
+
+        `R`" gas constant [J/kgK]
+        """
         self.gmma = gmma
         self.R = R
+        self.cp = (gmma*R)/(gmma-1)
+        self.cv = self.cp/R
     
+
     def ComputeStaticEnergy_p_rho(self, p, rho):
         return (p / (self.gmma - 1) / rho)
     
+
     def ComputePressure_rho_e(self, rho, e):
         return (self.gmma-1)*rho*e
     
+
     def ComputeSoundSpeed_p_rho(self, p, rho):
         return np.sqrt(self.gmma*p/rho)
     
+
     def ComputeStaticEnergy_u_et(self, vel, et):
         if isinstance(vel, np.ndarray):
             vel = np.linalg.norm(vel)
@@ -28,41 +44,49 @@ class FluidIdeal():
         e = et - 0.5*vel**2
         return e
     
+
     def ComputeSoundSpeed_rho_u_et(self, rho, u, et):
         e = self.ComputeStaticEnergy_u_et(u, et)
         p = self.ComputePressure_rho_e(rho, e)
         a = self.ComputeSoundSpeed_p_rho(p, rho)
         return a
     
+
     def ComputePressure_rho_u_et(self, rho, u, et):
         e = self.ComputeStaticEnergy_u_et(u, et)
         p = self.ComputePressure_rho_e(rho, e)
         return p
     
+    
     def ComputeTotalEnthalpy_rho_u_et(self, rho, u, et):
-        a = self.ComputeSoundSpeed_rho_u_et(rho, u, et)
-        umag = np.linalg.norm(u)
-        ht = a**2/(self.gmma-1)+0.5*umag**2
+        e = self.ComputeStaticEnergy_u_et(u, et)
+        p = self.ComputePressure_rho_e(rho, e)
+        ht = et+p/rho
         return ht
     
+
     def ComputeStaticPressure_pt_M(self, pt, M):
         p = pt*(1+(self.gmma-1)/2*M**2)**(-self.gmma/(self.gmma-1))
         return p 
     
+
     def ComputeStaticTemperature_rho_u_et(self, rho, u, et):
         p = self.ComputePressure_rho_u_et(rho, u, et)
         T = p/self.R/rho
         return T
     
+
     def ComputeStaticTemperature_Tt_M(self, Tt, M):
         T = Tt*(1+(self.gmma-1)/2*M**2)**(-1)
         return T
     
+
     def ComputeEntropy_rho_u_et(self, rho, u, et):
         p = self.ComputePressure_rho_u_et(rho, u, et)
         s = p/(rho**self.gmma)
         return s
     
+
     def ComputeDensity_p_T(self, p, T):
         rho = p/(self.R*T)
         return rho
