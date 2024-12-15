aa3ab7eeb59248a6a67f8a5e9e2010a8a343c3b8
Francesco Neri <francesconeri95@gmail.com>
2024-12-15 18:32:53 +0100

    update before adding blockage

---
+++ Run/Circular_Bump_BFM/post_process.py
@@ -0,0 +1,22 @@
+import numpy as np
+import pickle
+import os
+import matplotlib.pyplot as plt
+from TurboBFM.Postprocess.CPostProcess import CPostProcess
+
+sol_dir = 'Results'
+pik_files = [sol_dir + '/' + file for file in os.listdir(sol_dir) if file.endswith('.pik')]
+pik_files = sorted(pik_files)
+ 
+proc = CPostProcess(pik_files[-1])
+proc.PlotResiduals(save_filename='Residuals')
+proc.PlotMassFlow(save_filename='MassFlow')
+
+proc.Contour2D('rho', save_filename='Contour')
+proc.Contour2D('ux', save_filename='Contour')
+proc.Contour2D('uy', save_filename='Contour')
+proc.Contour2D('Mach', save_filename='Contour')
+proc.Contour2D('p', save_filename='Contour')
+
+plt.show()
+
